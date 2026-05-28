6b709dc1062b52b4b980f92b76c6c6281f8d2329
vdzhagaev <slava.dzhagaev@proton.me>
2026-05-28 13:41:55 +0300

    feat(application): extend WalletDetailsDto

--- BudgetTracker.Application/Wallets/Queries/GetWallet/WalletDetailsDto.cs
+++ BudgetTracker.Application/Wallets/Queries/GetWallet/WalletDetailsDto.cs
@@ -2,4 +2,13 @@ using BudgetTracker.Shared;
 
 namespace BudgetTracker.Application.Wallets.Queries.GetWallet;
 
-public sealed record WalletDetailsDto(Guid Id, string Name, string Currency, decimal Balance);
+public sealed record WalletDetailsDto(
+    Guid Id, 
+    string Name, 
+    string Currency, 
+    decimal Balance,
+    uint Version,
+    decimal IncomesTotal,
+    decimal ExpensesTotal,
+    int TransactionsTotal
+    );
