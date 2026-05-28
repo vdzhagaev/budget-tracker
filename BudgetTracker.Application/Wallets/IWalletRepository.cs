6502b6d4e731ad68cb33a995c61eba7b29abaaab
vdzhagaev <slava.dzhagaev@proton.me>
2026-05-28 16:57:47 +0300

    feat(application): update IWalletRepository: add new method Task<IReadOnlyList<Wallet>> ListAllAsync()

--- BudgetTracker.Application/Wallets/IWalletRepository.cs
+++ BudgetTracker.Application/Wallets/IWalletRepository.cs
@@ -4,6 +4,7 @@ namespace BudgetTracker.Application.Wallets;
 
 public interface IWalletRepository
 { 
+    Task<IReadOnlyList<Wallet>> ListAllAsync();
     Task<Wallet?> GetByIdAsync(Guid id, CancellationToken ct);
     Task AddAsync(Wallet wallet, CancellationToken ct);
     Task UpdateAsync(Wallet wallet, CancellationToken ct);
